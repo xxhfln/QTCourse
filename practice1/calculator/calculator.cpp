1d579bff44b0e64c596ab706e5742bb8702ca2c3
xxhfln <1564781600@qq.com>
2024-10-12 17:08:14 +0800

    添加LineEdit用于显示计算表达式

--- practice1/calculator/calculator.cpp
+++ practice1/calculator/calculator.cpp
@@ -1,11 +1,18 @@
 #include "calculator.h"
 #include "ui_calculator.h"
 
+#include <QRegExpValidator>
+
 Calculator::Calculator(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::Calculator)
 {
     ui->setupUi(this);
+    //只能输入数字
+    ui->show_number->setValidator(new QRegExpValidator(QRegExp("[0-9]+$")));
+
+    setWindowIcon(QIcon(":/images/images/calculator.png"));
+
 }
 
 Calculator::~Calculator()
